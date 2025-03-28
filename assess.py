5ee9fdf471a9cd88e1737b320595b97da0b5a38c
PalomoIFCA <palomo@ifca.unican.es>
2025-03-28 10:49:49 +0100

    changed implemetation for test

--- assess.py
+++ assess.py
@@ -323,6 +323,7 @@ def main():
         logger.info("SQAaaS assessment data obtained. Creating summary..")
         logger.debug(sqaaas_report_json)
         if only:
+           summary=('only one atribute')
            print('t')
         else:
            summary = write_summary(sqaaas_report_json)
